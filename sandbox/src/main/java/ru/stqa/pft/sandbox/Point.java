23ff48b015f4f33a0f501b878952ed3abc6ac3d2
Ankhez <79629070381@yandex.ru>
2016-03-26 23:15:24 +0300

    Point

--- sandbox/src/main/java/ru/stqa/pft/sandbox/Point.java
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/Point.java
@@ -4,15 +4,29 @@ package ru.stqa.pft.sandbox;
  * Created by vity on 26.03.2016.
  */
 public class Point {
- public double c,d;
+  public double getC() {
+    return c;
+  }
+
+  public double getD() {
+    return d;
+  }
+
+  public double c,d;
 
 
   public Point(double c,double d){
     this.c=c;
     this.d=d;
+
   }
-  public  double distance()
+
+
+
+  public double distance(Point p1)
   {
+
+    return Math.sqrt(Math.pow((this.c-p1.c),2)+ Math.pow((this.d-p1.d),2));
 
   }
 
--- sandbox/src/main/java/ru/stqa/pft/sandbox/Test.java
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/Test.java
@@ -11,17 +11,23 @@ public class Test {
 		Rectangle r=new Rectangle(4,5);*/
 		Point  p1=new Point(4,5);
 		Point  p2=new Point(7,3);
-		System.out.println(distance(p1,p2));
+		//System.out.println(distance(p1,p2));
+		System.out.println(p2.distance(p1));
+
+		p1.getC();
+		p1.getD();
+		System.out.println();
+
 
 
 
 		//System.out.println("Площадь прямоугольника со сторонами"+" "+r.a+" "+"и"+" "+r.b+"="+r.area());
 
 	}
-	public static double distance(Point p1, Point p2)
+/*	public static double distance(Point p1, Point p2)
 	{
 		return Math.sqrt(Math.pow((p2.c-p1.c),2)+ Math.pow((p2.d-p1.d),2));
-	}
+	}*/
 	public static void hello(String somebody)
 	{
 		System.out.println("Hello,"+somebody);
