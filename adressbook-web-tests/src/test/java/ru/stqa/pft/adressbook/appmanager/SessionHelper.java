064532fb2584a40e53a61e8d3e3b13a4d9084e35
Ankhez <79629070381@yandex.ru>
2016-04-03 20:11:28 +0300

    model+tests+appmanager

---
+++ adressbook-web-tests/src/test/java/ru/stqa/pft/adressbook/appmanager/SessionHelper.java
@@ -0,0 +1,23 @@
+package ru.stqa.pft.adressbook.appmanager;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+/**
+ * Created by vity on 03.04.2016.
+ */
+public class SessionHelper {
+  private FirefoxDriver wd;
+  public SessionHelper(FirefoxDriver wd) {
+    this.wd=wd;
+  }
+  public void login(String username, String password) {
+    wd.findElement(By.name("user")).click();
+    wd.findElement(By.name("user")).sendKeys(username);
+    wd.findElement(By.name("pass")).click();
+    wd.findElement(By.name("pass")).sendKeys(password);
+    wd.findElement(By.cssSelector("html")).click();
+    wd.findElement(By.xpath("//form[@id='LoginForm']/input[3]")).click();
+    wd.findElement(By.cssSelector("html")).click();
+  }
+}
