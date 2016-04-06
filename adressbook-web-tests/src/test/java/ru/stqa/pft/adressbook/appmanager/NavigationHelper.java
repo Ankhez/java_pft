ef5302a6df23267e7379a8215d7a494abd216678
Ankhez <79629070381@yandex.ru>
2016-04-06 23:01:44 +0300

    CHROME+FIREFOX

--- adressbook-web-tests/src/test/java/ru/stqa/pft/adressbook/appmanager/NavigationHelper.java
+++ adressbook-web-tests/src/test/java/ru/stqa/pft/adressbook/appmanager/NavigationHelper.java
@@ -1,6 +1,7 @@
 package ru.stqa.pft.adressbook.appmanager;
 
 import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import ru.stqa.pft.adressbook.model.GroupData;
 import ru.stqa.pft.adressbook.model.NewAddDate;
@@ -11,7 +12,7 @@ import ru.stqa.pft.adressbook.model.NewAddDate;
 public class NavigationHelper extends HelperBase{
 
 
-  public NavigationHelper(FirefoxDriver wd) {
+  public NavigationHelper(WebDriver wd) {
     super(wd);
 
   }
