f674c5bbed492f2d1925c5dc1f585c0defda8bc0
Ankhez <79629070381@yandex.ru>
2016-04-25 21:34:49 +0300

    Counts

--- adressbook-web-tests/src/test/java/ru/stqa/pft/adressbook/tests/GroupModificationTest.java
+++ adressbook-web-tests/src/test/java/ru/stqa/pft/adressbook/tests/GroupModificationTest.java
@@ -1,5 +1,6 @@
 package ru.stqa.pft.adressbook.tests;
 
+import org.testng.Assert;
 import org.testng.annotations.Test;
 import ru.stqa.pft.adressbook.model.GroupData;
 
@@ -10,6 +11,7 @@ public class GroupModificationTest extends TestBase {
   @Test
   public void testGroupModification(){
     app.getNavigationHelper().gotoGroupPage();
+    int before = app.getGroupHelper().getGroupCount();
     if(! app.getGroupHelper().isThereAGroup()) {
       app.getGroupHelper().createGroup(new GroupData("test1", null, null));
     }
@@ -18,6 +20,8 @@ public class GroupModificationTest extends TestBase {
     app.getGroupHelper().fillGroupForm(new GroupData("test1", "test2", "test3"));
     app.getGroupHelper().submitGroupModifictaion();
     app.getGroupHelper().returnToGroupPage();
+    int after = app.getGroupHelper().getGroupCount();
+    Assert.assertEquals(after,before );
 
   }
 }
